e879f893915349815daf48cbfbb3c07e9e110661
Angelo Romano <angelorj97@hotmail.com>
2020-11-02 22:31:07 -0300

    Summary View displays database info Added new routes to main.js Fixed minor pug views bugs

--- routes/main.js
+++ routes/main.js
@@ -2,9 +2,49 @@ const express = require('express');
 const passport = require('passport');
 const router = express.Router();
 
+//AWS Settings
+var aws = require("aws-sdk");
+var db = require("../db");
+aws.config.update({
+  region: process.env.DBREGION,
+  endpoint: process.env.ENDPOINT,
+  accessKeyId: process.env.AKID,
+  secretAccessKey: process.env.SECRET
+});
+
 /* GET home page. */
 router.get('/',passport.authenticate('jwt', {session: false, failureRedirect: '/login'}),  async(req, res) => {
   res.render('index', { title: 'NVIO' });
+});
+
+router.get('/profile',passport.authenticate('jwt', {session: false, failureRedirect: '/login'}),  async(req, res) => {
+  res.render('profile', { title: 'NVIO' });
+});
+
+/* GET historial. */
+router.get('/historial',passport.authenticate('jwt', {session: false, failureRedirect: '/login'}),  async(req, res) => {
+  console.log("Historial requested")
+  const name = "Historial";
+  var params={
+    "TableName": "app",
+    "ScanIndexForward": false,
+    "ConsistentRead": false,
+    "KeyConditionExpression": "#cd420 = :cd420 And begins_with(#cd421, :cd421)",
+    "ExpressionAttributeValues": {
+      ":cd420": {
+        "S": req.user.user
+      },
+      ":cd421": {
+        "S": "ORDER"
+      }
+    },
+    "ExpressionAttributeNames": {
+      "#cd420": "PK",
+      "#cd421": "SK"
+    }
+  };
+  historialQuery = await db.query(params);
+  res.render('historial', {title: name, orders: historialQuery.Items, companyId: req.user.user.replace("COMPANY#","")});
 });
 
 //Login route
