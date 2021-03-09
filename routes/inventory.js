7b513260eef3cf428081003b2984a85e5843dc84
Eduardo Pérez <eduardojoseperezf@gmail.com>
2021-03-09 11:42:37 -0300

    Basic elasticsearch routing + search

---
+++ routes/inventory.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const router = express.Router();
+const passport = require('passport');
+const validator = require('validator');
+const { nanoid } = require("nanoid");
+const { Client } = require('@elastic/elasticsearch')
+const client = new Client({
+  node: process.env.ELASTIC_ENDPOINT,
+  auth: {
+    username: process.env.ELASTIC_USERNAME,
+    password: process.env.ELASTIC_PASSWORD
+  }
+})
+
+router.get('/',passport.authenticate('jwt', {session: false, failureRedirect: '/login'}),  async(req, res) => {
+  const result = await client.search({
+    index: 'products',
+    body: {
+      query: {
+        match: { fullname: 'Polera roja' }
+      }
+    }
+  })
+  console.log(result.body.hits.hits);
+
+  res.status(200).json(result.body.hits.hits)
+});
+
+router.get('/searchProduct/:fullname',passport.authenticate('jwt', {session: false, failureRedirect: '/login'}),  async(req, res) => {
+  const result = await client.search({
+    index: 'products',
+    body: {
+      query: {
+        match: { fullname: req.params.fullname }
+      }
+    }
+  })
+  console.log(result.body.hits.hits);
+
+  res.status(200).json(result.body.hits.hits)
+});
+
+
+module.exports = router;
