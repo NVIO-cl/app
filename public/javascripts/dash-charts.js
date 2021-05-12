cb4028b1e5ce9f98993cd63851349f05053cb701
Gonzalo Vidal <31936697+GonzoVF@users.noreply.github.com>
2021-05-12 18:24:23 -0400

    Tweaked Old Dashboard Layout
    - Returned to the dashboard layout shown in promo material
    - Tweaked that Layout
    - Moved style definitions to dash-charts.js

--- public/javascripts/dash-charts.js
+++ public/javascripts/dash-charts.js
@@ -1,113 +1,176 @@
-Chart.defaults.global.defaultFontFamily= 'Poppins'
-
-const topSellersCtx = document.getElementById('topSellers').getContext('2d');
-const topSellers = new Chart(topSellersCtx, {
-  type: 'bar',
-  data: {
-      labels: ['Galletas Chocolate', 'Alfajores', 'Tortas', 'Chocolate Blanco', 'Mazapán'],
-      datasets: [{
-          label: '# of Sales',
-          data: [12, 19, 3, 5, 2, 0],
-          backgroundColor: 'rgba(8, 97, 255, 0.2)',
-          borderWidth: 0,
-          hoverBorderColor: 'rgb(8, 97, 255)',
-          hoverBorderWidth: 1
-      }]
-  },
-  options: {
-      responsive: true,
-      scales: {
-        x: {
-          color: 'rgba(0, 0, 0, 0)'
-        },
-        y: {
-          beginAtZero: true
-        },
-        min: 0
-      },
-  }
-});
-
-const salesBreakdownCtx = document.getElementById('salesBreakdown').getContext('2d');
-const salesBreakdown = new Chart(salesBreakdownCtx, {
-  type: 'line',
-  data: {
-      labels: ['Ene', 'Feb', 'Mar', 'Abr', 'Jun', 'Jul'],
-      datasets: [{
-          label: '# of Sales',
-          data: [12, 19, 3, 5, 2, 18, 0],
-          backgroundColor: 'rgba(8, 97, 255, 0.2)',
-          borderWidth: 0,
-          hoverBorderColor: 'rgb(8, 97, 255)',
-          hoverBorderWidth: 1
-      }]
-  },
-  options: {
-      responsive: true,
-      scales: {
-        x: {
-          color: 'rgba(0, 0, 0, 0)'
-        },
-        y: {
-          beginAtZero: true
-        },
-        min: 0
-      },
-  }
-});
+Chart.defaults.global.defaultFontFamily= 'Nunito'
 
 const perLocalityCtx = document.getElementById('perLocality').getContext('2d');
 const perLocality = new Chart(perLocalityCtx, {
-  type: 'pie',
-  data: {
-      labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun'],
-      datasets: [{
-          label: '# of Sales',
-          data: [7, 1, 3, 5, 2, 3],
-          backgroundColor: 'rgba(8, 97, 255, 0.2)',
-          borderColor: '#fff',
-          borderWidth: 2,
-          hoverBackgroundColor: 'rgb(8, 97, 255)',
-          hoverBorderWidth: 0
-      }]
+  type:"doughnut",
+  data:{
+    labels:["Direct","Social","Referral"],
+    datasets:[{
+      label:"Channel",
+      data:["5","30","15"],
+      backgroundColor:["#0861ff","#0d87f0","#03f5f5"],
+      borderColor: "#fff",
+      borderWidth: 3,
+      hoverBorderWidth: 0
+    }]
   },
-  options: {
-      responsive: true,
-      scales: {
-        x: {
-        },
-        y: {
-          beginAtZero: true
-        },
-        min: 0
-      },
+  options:{
+    maintainAspectRatio:false,
+    legend:{
+      display:false
+    }
   }
-});
+  });
 
-const ordersBreakdownCtx = document.getElementById('ordersBreakdown').getContext('2d');
-const ordersBreakdown = new Chart(ordersBreakdownCtx, {
-  type: 'line',
-  data: {
-      labels: ['Ene', 'Feb', 'Mar', 'Abr', 'Jun', 'Jul'],
-      datasets: [{
-          label: '# of Sales',
-          data: [12, 19, 3, 5, 2, 18, 0],
-          backgroundColor: 'rgba(8, 97, 255, 0.2)',
-          borderWidth: 0,
-          hoverBorderColor: 'rgb(8, 97, 255)',
-          hoverBorderWidth: 1
-      }]
+const perPeriodCtx = document.getElementById('perPeriod').getContext('2d');
+const perPeriod = new Chart(perPeriodCtx, {
+  type:"line",
+  data:{
+    labels:["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug"],
+    datasets:[{
+      label:"Earnings",
+      fill:true,
+      data:["37000","10000","8000","4000","7600","2050","7000","2000"],
+      backgroundColor:"rgba(78, 115, 223, 0.05)",
+      borderColor:"#0861ff"
+    }]
   },
-  options: {
-      responsive: true,
-      scales: {
-        x: {
-          color: 'rgba(0, 0, 0, 0)'
-        },
-        y: {
-          beginAtZero: true
-        },
-        min: 0
+  options:{
+    maintainAspectRatio:false,
+    legend:{
+      display:false
+    },
+    title:{},
+    scales:{
+      xAxes:[{
+        gridLines:{
+          color:"rgb(234, 236, 244)",
+          zeroLineColor:"rgb(234, 236, 244)",
+          drawBorder:false,
+          drawTicks:false,
+          borderDash:["2"],
+          zeroLineBorderDash:["2"],
+          drawOnChartArea:false},
+          ticks:{
+            fontColor:"#858796",
+            padding:20
+          }
+        }],
+        yAxes:[{
+          gridLines:{
+            color:"rgb(234, 236, 244)",
+            zeroLineColor:"rgb(234, 236, 244)",
+            drawBorder:false,
+            drawTicks:false,
+            borderDash:["2"],
+            zeroLineBorderDash:["2"]},
+            ticks:{
+              fontColor:"#858796",
+              padding:20
+            }
+          }]
+        }
+      }
+    });
+
+const topSellersCtx = document.getElementById('topSellers').getContext('2d');
+const topSellers = new Chart(topSellersCtx, {
+  type:"bar",
+  data:{
+    labels:["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug"],
+    datasets:[{
+      label:"Earnings",
+      fill:true,
+      data:["37000","10000","8000","4000","7600","2050","7000","2000"],
+      backgroundColor:"#0861ff",
+      borderWidth: 0
+    }]
+  },
+  options:{
+    maintainAspectRatio:false,
+    legend:{
+      display:false
+    },
+    title:{},
+    scales:{
+      xAxes:[{
+        gridLines:{
+          color:"rgb(234, 236, 244)",
+          zeroLineColor:"rgb(234, 236, 244)",
+          drawBorder:false,
+          drawTicks:false,
+          borderDash:["2"],
+          zeroLineBorderDash:["2"],
+          drawOnChartArea:false},
+          ticks:{
+            fontColor:"#858796",
+            padding:20
+          }
+        }],
+        yAxes:[{
+          gridLines:{
+            color:"rgb(234, 236, 244)",
+            zeroLineColor:"rgb(234, 236, 244)",
+            drawBorder:false,
+            drawTicks:false,
+            borderDash:["2"],
+            zeroLineBorderDash:["2"]},
+            ticks:{
+              fontColor:"#858796",
+              padding:20
+            }
+          }]
+        }
+      }
+    });
+
+    const trackMonthCtx = document.getElementById('trackMonth').getContext('2d');
+    const trackMonth = new Chart(trackMonthCtx, {
+      type:"line",
+      data:{
+        labels:["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug"],
+        datasets:[{
+          label:"Earnings",
+          fill:true,
+          data:["3700","100","8000","4000","7600","2050","7000","2000"],
+          backgroundColor:"rgba(78, 115, 223, 0.05)",
+          borderColor:"#0861ff"
+        }]
       },
-  }
-});
+      options:{
+        maintainAspectRatio:false,
+        legend:{
+          display:false
+        },
+        title:{},
+        scales:{
+          xAxes:[{
+            gridLines:{
+              color:"rgb(234, 236, 244)",
+              zeroLineColor:"rgb(234, 236, 244)",
+              drawBorder:false,
+              drawTicks:false,
+              borderDash:["2"],
+              zeroLineBorderDash:["2"],
+              drawOnChartArea:false},
+              ticks:{
+                fontColor:"#858796",
+                padding:20
+              }
+            }],
+            yAxes:[{
+              gridLines:{
+                color:"rgb(234, 236, 244)",
+                zeroLineColor:"rgb(234, 236, 244)",
+                drawBorder:false,
+                drawTicks:false,
+                borderDash:["2"],
+                zeroLineBorderDash:["2"]},
+                ticks:{
+                  fontColor:"#858796",
+                  padding:20
+                }
+              }]
+            }
+          }
+        });
